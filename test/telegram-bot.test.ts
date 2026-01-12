40f73123900bdf1dd733a5c20f68a76f87441c13
AARAQUE <aaraque@bancodeoccidente.com.co>
2026-01-12 18:12:57 -0500

    Initial commit

---
+++ test/telegram-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TelegramBot from '../lib/telegram-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/telegram-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TelegramBot.TelegramBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
